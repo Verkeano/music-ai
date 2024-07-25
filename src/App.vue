92057241e3239773058ff237404ab4f92b71a26b
Verkeano <Alanhild715@gmail.com>
2024-07-25 14:36:15 +0100

    Design

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,25 @@
 <template>
   <main>
-
+    <main>
+    <body>
+      <div className="App">
+        <header class="App-header">
+          <h1>LyricAI</h1>
+          <h6>Search with lyrics</h6>
+        </header>	
+        <header class="form">
+          <textarea bind:value={Lyrics}  class="search-for-lyrics" cols="30" rows="10" type="text"/>
+          <button type="submit">Search</button>
+        </header>
+        <h6>{Lyrics}</h6>
+      </div>
+    </body>
+    </main>
   </main>
 </template>
 
 <script>
+let Lyrics = "";
 export default {
   name: 'App',
 }
